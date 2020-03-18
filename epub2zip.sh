599fa4e1098bce05a82e6452363b0ebba1a07d8a
weich <weichunhao@yzhtech.com>
2020-03-18 10:45:57 +0800

    * 格式化图片名 * 读取原有图片格式

--- epub2zip.sh
+++ epub2zip.sh
@@ -21,17 +21,21 @@ for file in $(find . -name "*.epub"); do
         mkdir ${zipPath}
     fi
 
-    unzip -d ${tmpPath} ${file} > /dev/null
+    unzip -d ${tmpPath} ${file} >/dev/null
 
     htmlPath="${tmpPath}/html"
-    index=1 
+    index=1
     while ([ -f "./${htmlPath}/$index.html" ]); do
         # echo ${htmlPath}/$index.html
         IMGURL=$(cat ${htmlPath}/${index}.html | grep img | cut -d '"' -f 2)
         # echo ${IMGURL}
 
         oldImgPath=${tmpPath}/${IMGURL#../}
-        newImgPath=${zipPath}/${index}.jpg
+
+        array=(${oldImgPath//./ })
+        typeName=${array[${#array[@]} - 1]}
+        newImgPath=${zipPath}/$(echo ${index} | awk '{printf("%05d",$0)}').${typeName}
+
         mv ${oldImgPath} ${newImgPath}
 
         index=$(($index + 1))
@@ -40,10 +44,9 @@ for file in $(find . -name "*.epub"); do
     cd ${zipPath}
     zip -q ../${filename}.zip *
     cd ..
-    
+
     rm -rf ${tmpPath}
     rm -rf ${zipPath}
 
     echo ${zipPath}.zip
 done
-
